5a81890561c27b9d0956f8839b6a42c62211e5fa
it21013928 <it21013928@my.sliit.lk>
2023-03-01 13:19:36 +0530

    Complete promises,Async/Await

--- app.js
+++ app.js
@@ -37,3 +37,28 @@ https
 
 const myModule = require("./my-module.js");
 console.log(myModule.myFunction());
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve("Success!");
+  } else {
+    reject("Failure!");
+  }
+});
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+myFunction();
